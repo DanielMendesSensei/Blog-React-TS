6271aca6a53d8047ca32bfd6b89b330ab0a255cf
Daniel Mendes <senseidanielmendes@gmail.com>
2024-04-02 16:27:00 -0300

    fix: Skeleton.tsx type

--- src/components/Skeletons/Skeleton.tsx
+++ src/components/Skeletons/Skeleton.tsx
@@ -1,7 +1,13 @@
 import "./Skeleton.css";
 
-const Skeleton = ({ classes }): JSX.Element => {
-  const classNames = `skeleton ${classes} animate-pulse`;
+interface SkeletonProps {
+  classes: string | string[];
+}
+
+const Skeleton = ({ classes }: SkeletonProps): JSX.Element => {
+  const classNames = `skeleton ${
+    Array.isArray(classes) ? classes.join(" ") : classes
+  } animate-pulse`;
 
   return <div className={classNames}></div>;
 };
